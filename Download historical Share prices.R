807130ddae33390a6cb35a5dfefb93aa2a7d7548
Sulthan .A <sulthan90@gmail.com>
2016-10-30 14:44:11 +0530

    Update Download historical Share prices.R

--- Download historical Share prices.R
+++ Download historical Share prices.R
@@ -1,20 +1,20 @@
 ####Using quantmod####
 ####Single Stock data####
 if(!require(quantmod)){install.packages("quantmod")}
-getSymbols("WIPRO.Ns", from="2015-01-01", to="2016-10-20")
+getSymbols("WIPRO.Ns", from="2015-01-01", to= Sys.Date())
 
 #### multiple Stocks data Method-1 (each as seperate dataset) ####
-getSymbols("WIPRO.NS;TCS.NS;INFY.NS", from="2015-01-01", to="2016-10-20")
+getSymbols("WIPRO.NS;TCS.NS;INFY.NS", from="2015-01-01", to= sys.Date())
 
 #### multiple Stocks Method-2 (using list)####
-stocklist <- c("WIPRO.NS","TCS.NS","INFY.NS","AAPL")
-getSymbols(stocklist, from="2015-01-01", to="2016-10-20")
+stocklist <- c("WIPRO.NS","TCS.NS","INFY.NS","AAPL" )
+getSymbols(stocklist, from="2015-01-01", to= Sys.Date())
 
 ####multiple Stocks Method-3####
 stocklist <- c("WIPRO.NS","TCS.NS","INFY.NS","AAPL") # create list of stock tickers
 Adjclose <- NULL
 for (Ticker in stocklist)
-  Adjclose <- cbind(Adjclose,getSymbols.yahoo(Ticker, from="2015-01-01", verbose=FALSE, auto.assign=FALSE)[,6]) #get data for companies in list, [,6] = keep the adjusted prices
+  Adjclose <- cbind(Adjclose,getSymbols.yahoo(Ticker, from="2015-01-01", to= Sys.Date(), verbose=FALSE, auto.assign=FALSE)[,6]) #get data for companies in list, [,6] = keep the adjusted prices
 FinalAdjclose <- na.locf(Adjclose) #copy last traded price in empty cell
 FinalAdjclose1 <- Adjclose[apply(Adjclose,1,function(x) all(!is.na(x))),] #keep only dates having closing price
 ####Export the data to excel####
